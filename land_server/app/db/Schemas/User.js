fb614af5c60f08e9bc2bd2e2a17fc81bec3ee884
RachellmNan <826015755@qq.com>
2021-07-06 20:22:15 +0800

    密码加密

---
+++ land_server/app/db/Schemas/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+var bcrypt = require('bcryptjs');
+const { getMeta, preSave } = require('../helpers')
+
+const UserSchama = new mongoose.Schema({
+    nickname: String,
+    password: {
+        type: String,
+        set: val=>{
+            const salt = bcrypt.genSaltSync(10)
+            const hash = bcrypt.hashSync(val, salt)
+            return hash
+        }
+    },
+    email: String,
+    openid: String,
+    meta: getMeta()
+})
+UserSchama.pre('save',preSave)
+mongoose.model('User',UserSchama)
