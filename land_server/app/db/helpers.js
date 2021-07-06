dbb3d855778498af245d5e96231ab8c6b67631da
RachellmNan <826015755@qq.com>
2021-07-06 16:03:54 +0800

    promise 封装数据库

---
+++ land_server/app/db/helpers.js
@@ -0,0 +1,28 @@
+const getMeta = ()=>{
+    return {
+        createAt:{
+            type:Number,
+            default:(new Date()).getTime()
+        },
+        updatedAt:{
+            type:Number,
+            default:(new Date()).getTime()
+        }
+    }
+}
+
+const preSave = function (next) {
+    if(this.isNew){
+        const ts = Date.now()
+        this['meta'].createAt = ts
+        this['meta'].updatedAt = ts
+    }else{
+        this['meta'].updatedAt = Date.now()
+    }
+    next()
+}
+
+module.exports = {
+    getMeta,
+    preSave
+}
