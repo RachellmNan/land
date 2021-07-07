156d2a9b4fb1ece056f1e43b5099d0f96039de39
RachellmNan <826015755@qq.com>
2021-07-07 22:00:44 +0800

    异常封装

--- land_server/core/http-exception.js
+++ land_server/core/http-exception.js
@@ -16,7 +16,57 @@ class ParameterException extends HttpException{
     }
 }
 
+class NotFound extends HttpException {
+    constructor(msg, errorCode) {
+        super()
+        this.status = 404
+        this.msg = msg || '资源未找到'
+        this.errorCode = errorCode || 10000
+    }
+}
+
+class AuthFailed extends HttpException {
+    constructor(msg, errorCode) {
+        super()
+        this.status = 401
+        this.msg = msg || '授权失败'
+        this.errorCode = errorCode || 10004
+    }
+}
+
+class Forbidden extends HttpException {
+    constructor(msg, errorCode) {
+        super()
+        this.status = 403
+        this.msg = msg || '禁止访问'
+        this.errorCode = errorCode || 10006
+    }
+}
+
+class linkError extends HttpException {
+    constructor(msg, errorCode) {
+        super()
+        this.status = 400
+        this.msg = msg || '你已经点赞过'
+        this.errorCode = errorCode || 60001
+    }
+}
+
+class DislikeError extends HttpException {
+    constructor(msg, errorCode) {
+        super()
+        this.code = 400
+        this.msg = msg || '你已取消点赞'
+        this.errorCode = errorCode || 60002
+    }
+}
+
 module.exports = {
     HttpException,
-    ParameterException
+    ParameterException,
+    NotFound,
+    AuthFailed,
+    Forbidden,
+    linkError,
+    DislikeError
 }
