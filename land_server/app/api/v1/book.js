a10acb08a6ab1e75536a7444f3a360ed27d8a229
RachellmNan <826015755@qq.com>
2021-07-08 22:41:30 +0800

    业务开发

--- land_server/app/api/v1/book.js
+++ land_server/app/api/v1/book.js
@@ -1,28 +1,20 @@
 const Router = require('koa-router')
 const {HttpException, ParameterException} = require('../../../core/http-exception')
+const validator = require('validator'); 
+const Auth = require('../../../middlewares/auth');
 const router = new Router()
 
-router.post('/v1/:id/book',(ctx,next)=>{
-    ctx.body = 'this is book'
-    console.log('this is book')
-    const params = ctx.params
-    const query = ctx.query
-    const header = ctx.header
-    const body = ctx.request.body
-    console.log('path: ',ctx.path)
-    console.log('params: ',params)
-    console.log('query: ',query)
-    console.log('header: ',header)
-    console.log('body: ',body)
-    console.log('Http: ',ctx.status)
-    if(true){
-        console.log('进入了 ')
-        const error = new ParameterException()
-        throw error
-    }
-    ctx.body = {
-        "success":200
-    }
+router.post('/v1/book',new Auth(Auth.ADMIN).verify, (ctx,next)=>{
+
+    // console.log('this is book')
+    // const params = ctx.params
+    // const query = ctx.query
+    // const header = ctx.header
+    // const body = ctx.request.body
+    // if(!validator.isInt(params.id, {min:1})){
+    //     throw new ParameterException()
+    // }
+    ctx.body = ctx.auth
 })
 
 
