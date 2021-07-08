8fd1f27394181ccff05056526f3ef7046edd1e51
RachellmNan <826015755@qq.com>
2021-07-08 19:55:04 +0800

    jwt认证

---
+++ land_server/middlewares/auth.js
@@ -0,0 +1,42 @@
+const basicAuth = require('basic-auth')
+const config = require('../config/config')
+const jwt = require('jsonwebtoken')
+const { Forbidden } = require('../core/http-exception')
+
+class Auth {
+    constructor(level){
+        this.level = level || 1 // api 的访问level
+    }
+    static USER = 8  // 用户的级别
+    static ADMIN = 16 // 管理员的级别
+    static SUPER_ADMIN = 32 // 超级管理员的级别
+    get verify(){
+        return async (ctx,next)=>{
+            const AuthObj = basicAuth(ctx.req)
+            let decode
+            let errMsg = 'token不合法'
+            if(!AuthObj || !AuthObj.name){
+                throw new Forbidden(errMsg)
+            }
+            try {
+                decode = jwt.verify(AuthObj.name, config.security.secretKey)
+            } catch (error) {
+                if(error.name == 'TokenExpiredError'){
+                    errMsg = 'token已过期'
+                }
+                throw new Forbidden(errMsg)
+            }
+            // 该用户的权限是否能访问该 api
+            if(decode.scope < this.level){
+                throw new Forbidden('权限不足')
+            }
+            ctx.auth = {
+                uid: decode.uid,
+                scope: decode.scope
+            }
+            await next()
+        }
+    }
+}
+
+module.exports = Auth
