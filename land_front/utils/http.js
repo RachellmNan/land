e24192a632d2e16a5a344268dc6e1f9762b54e4b
RachellmNan <826015755@qq.com>
2021-10-09 22:22:27 +0800

    二次重发

--- land_front/utils/http.js
+++ land_front/utils/http.js
@@ -1,6 +1,7 @@
 const config = require("../config")
-const { promisic } = require("./util")
+const { _promisic } = require("./util")
 import { Base64 } from 'js-base64'
+const { Storage } = require('../models/storage')
 
 function _encode() {
     const token = wx.getStorageSync('token')
@@ -9,19 +10,10 @@ function _encode() {
 }
 
 class Http{
-    async request({url, data = {}, method = 'GET'}){
-        return await promisic(wx.request)({
-            header:{
-                appkey: config.appkey
-            },
-            url: config.api_base_url + url,
-            method,
-            data
-        })
-    }
-    async _request({url, data = {}, method = 'GET'}){
-        console.log('此次请求的url: ',config.m_api_base_url + url)
-            return await promisic(wx.request)({
+    async _request({url, data = {}, method = 'GET', refetch = true}){
+        let res
+        try {
+            res = await _promisic(wx.request)({
                 header:{
                     'content-type': 'application/json',
                     Authorization: _encode()
@@ -30,8 +22,59 @@ class Http{
                 method,
                 data
             })
-        
+        } catch (err) {
+            _showErr(1)
+            throw new Error(err.errMsg)
+        }
+        let code = res.statusCode.toString()
+        if(code.startsWith('2')){
+            return res.data
+        }else if(code == '403' && res.data.msg == 'token已过期'){
+            if(refetch){
+                
+                return await Http._refetch({
+                    url,
+                    data,
+                    method,
+                    refetch:false
+                })
+            }
+        }
+        _showErr(1)
+        throw new Error(res.data.msg)
     }
+
+    static async _refetch(param) {
+        let wx_code = await _promisic(wx.login)({})
+        let tokenObj =  await getApp().globalData.http._request({
+            url: '/token',
+            method:'POST',
+            data:{
+                code: wx_code.code
+            },
+            refetch: false
+        })
+        let token = tokenObj.token
+        Storage.setItem('token',token)
+        return await getApp().globalData.http._request(param)
+    }
+}
+
+
+
+const tips = {
+    '1' : '抱歉出现了一个错误',
+    '1005': 'appkey无效',
+    '3000': '期刊不存在'
+}
+
+function _showErr(code = 1){
+    const tip = tips[code]
+    wx.showToast({
+        title: tip?tip:tips[1],
+        duration:2000,
+        icon:"none"
+    })
 }
 
 module.exports = {
--- land_front/utils/util.js
+++ land_front/utils/util.js
@@ -1,47 +1,13 @@
 // 处理和服务器的网络请求
-const config = require('../config')
-// const { Http } = require('./http')
-// const { test } = require('./test')
-const { Storage } = require('../models/storage')
-
-function promisic(fn){
-    return  function(params){
-        return new Promise((reslve, reject)=>{
-            Object.assign(params,{
-                success:res=> {
-                    let code = res.statusCode.toString()
-                    console.log('返回结果: ',res)
-                    if(code.startsWith('2')){
-                        reslve(res.data)
-                    }else{
-                        if(res.data.msg == 'token已过期' && res.statusCode == '403'){
-                            console.log(3)
-                            wx.login({
-                                success:async (res)=>{
-                                    console.log(res)
-                                    let tokenObj =  await getApp().globalData.http._request({
-                                        url: '/token',
-                                        method:'POST',
-                                        data:{
-                                            code: res.code
-                                        }
-                                    })
-                                    console.log('令牌刷新', tokenObj)
-                                    let token = tokenObj.token
-                                    Storage.setItem('token',token)
-                                }
-                            })
-                            return
-                        }
-                        reject()
-                        let err_code = res.data.error_code
-                        _showErr(err_code)
-                    }
+function _promisic(fn) {
+    return function (params) {
+        return new Promise((resolve, reject)=>{
+            Object.assign(params, {
+                success: res =>{
+                    resolve(res)
                 },
-                fail(err){
-                    console.log(22)
+                fail: err=>{
                     reject(err)
-                    _showErr(1)
                 }
             })
             fn(params)
@@ -49,20 +15,6 @@ function promisic(fn){
     }
 }
 
-const tips = {
-    '1' : '抱歉出现了一个错误',
-    '1005': 'appkey无效',
-    '3000': '期刊不存在'
-}
-
-function _showErr(code = 1){
-    const tip = tips[code]
-    wx.showToast({
-        title: tip?tip:tips[1],
-        duration:2000,
-        icon:"none"
-    })
-}
 
 function getHeight(height = 0){
     let res = wx.getSystemInfoSync()
@@ -75,6 +27,6 @@ function px2rpx(scrrenWidth, scrrenHeight){
 
 
 module.exports = {
-    promisic,
+    _promisic,
     getHeight
 }
